4674f7c43e901dd01185b47df0423627c4312594
Ron Clark <ronaldtclark@gmail.com>
2018-06-22 14:41:56 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -12,9 +12,17 @@ $container = $('#container')
 $h1 = $('<h1>Hogwarts</h1>')
 $container.append($h1)
 
+// YEAR 2
 
-
-
+$h2 = $('<h2>Victor Vivicus</h2>')
+$container.append($h2)
+$h3 = $('<h3>Ravenclaw</h3>')
+$container.append($h3)
+$h4 = $('<h4>Badger</h4>')
+$h4.addClass('owl')
+$container.append($h4)
+$h4_2 = $('<h4>Elderwood Wand with Phoenix Feather Core</h4>')
+$container.append($h4_2)
 
 
 
