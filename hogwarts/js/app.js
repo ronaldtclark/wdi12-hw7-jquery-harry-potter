4184f82231b5ae3e8a56b4abc811d9110e63cf8f
Ron Clark <ronaldtclark@gmail.com>
2018-06-22 10:07:09 -0500

    Commit 1 - I added my first element and it was fun

---
+++ hogwarts/js/app.js
@@ -0,0 +1,44 @@
+if (typeof jQuery == 'undefined'){
+  console.log('oops! I still have to link my jQuery properly!');
+} else {
+  console.log('I did it! I linked jQuery and this js file!')
+};
+
+
+// YEAR 1
+
+$container = $('#container')
+
+$h1 = $('<h1>Hogwarts</h1>')
+$container.append($h1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
